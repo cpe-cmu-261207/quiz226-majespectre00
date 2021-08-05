927564356f8f404b72d3ee6dc652181f415f55e8
Sa <oat601@gmail.com>
2021-08-05 14:58:29 +0700

    up

--- script.js
+++ script.js
@@ -3,11 +3,38 @@ const calculation = document.querySelector("#calculation")
 const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
-
-// define more constants and variables here
+const btn_res = document.querySelector("#reset")
+const btn_search = document.querySelector("#search")
+const texttie = document.querySelector("#text")
+let toggle = false
+var code = parseInt(author.innerHTML.split(" ")[0])
+var le = parseInt(length.value)
+var calc = code + le
+ogauthor = author.innerText
 
 btn_toggle.onclick = () => {
-  // your code here
+  if(!toggle){
+  author.innerText = calc
+  btn_toggle.innerText = "Show Author"
+  toggle = true
+}else{
+  author.innerText = ogauthor
+  btn_toggle.innerText = "Show Calculation"
+    toggle = false
+  }
 }
+btn_res.onclick = () =>{
+  location.reload()
+}
+/*
+btn_search.onclick = ()=>{
+  let text = texttie.innerText.split(" ") 
+  for(x of text){
+    if(x.length >= le)
+    x = "<span style ="color:" + color +">"" x + "</span>"
+  }
+}
+*/
+
 
 // more codes for Search and Reset buttons here
